8ed6b971205da4a1aa926ec77666212066c04764
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-18 19:50:41 +0700

    function searchData

--- test.cpp
+++ test.cpp
@@ -96,3 +96,28 @@ void traverse() {
 		}
 	}
 }
+
+void searchData() {
+	if (listEmpty()) {
+		cout << "LIst Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		int nim;
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM : " << currentNode->noMhs << ", Nama : " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak ditemukan" << endl;
+	}
+	
+}
+
